e22fd68fe9c303ac79b0a50dc6468b20758fc58a
malinthann <malinthann99@gmail.com>
2024-02-14 16:24:13 +0700

    f-string

--- Day-01/input.py
+++ Day-01/input.py
@@ -15,3 +15,11 @@ print(name2) #This line will only print the name on line 2 of the input pane.
 name3 = input()
 print(name3)
 
+num_1 = 5
+
+num_2 = 7
+
+result = num_1 + num_2
+
+#use f-string to print the result
+print(f"The result is {num_1} + {num_2} = {result}")
