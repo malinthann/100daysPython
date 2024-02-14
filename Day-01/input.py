af1edb1f698d137646c33a48ef5cdf4ff6b32916
malinthann <malinthann99@gmail.com>
2024-02-14 10:54:01 +0700

    day1

---
+++ Day-01/input.py
@@ -0,0 +1,17 @@
+# input() function is used to take input from the user
+input("Hello, what is your name?")
+
+# input() can be used in print() function
+print("Hello, " + input("What is your name?") + "!")
+
+name1 = input()
+name2 = input()
+name3 = input()
+
+name1 = input("What is your name? ")
+print(name1) #This line will print the text and line 1 of the input into the output.
+name2 = input()
+print(name2) #This line will only print the name on line 2 of the input pane.
+name3 = input()
+print(name3)
+
---
+++ Day-01/print.py
@@ -0,0 +1,19 @@
+#Print Hello world
+print("Hello world")
+
+#add space between Hello and Malin
+print("Hello" + " " + "Malin")
+
+#add space on left sides of Malin
+print("Hello" + " Malin")
+
+#add space on right sides of Hello
+print("Hello " + "Malin")
+
+#single quote should be used before double quote
+
+#Example 
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
